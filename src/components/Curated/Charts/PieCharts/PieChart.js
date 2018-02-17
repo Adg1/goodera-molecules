ddeb130f048eb5e63a87f50f340bccbcb64d6a28
Adg1 <amandeepgupta1@gmail.com>
2018-02-17 13:17:47 +0530

    PieChart props

--- src/components/Curated/Charts/PieCharts/PieChart.js
+++ src/components/Curated/Charts/PieCharts/PieChart.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { VictoryPie } from 'victory';
+import PropTypes from 'prop-types';
 
 export default class PieChart extends Component {
-  render() {
-    const style = {
+
+  static propTypes = {
+    /** Piechart data; Array of ```{x:xLabel, y:yval}``` values */
+    data: PropTypes.array.isRequired,
+    /** Pie styles */
+    pieStyle: PropTypes.object,
+  }
+  static defaultProps = {
+    pieStyle: {
       parent: { border: "1px solid #ccc", margin: "2%", maxWidth: "60%" }
-    };
+    }
+  }
+
+  render() {
     return (
-      <VictoryPie style={style}/>
+      <VictoryPie style={this.props.pieStyle} data={this.props.data}/>
     );
   }
 }
--- src/components/Curated/Charts/PieCharts/Readme.md
+++ src/components/Curated/Charts/PieCharts/Readme.md
@@ -1,5 +1,11 @@
 Example of Pie Chart:
 
 ```jsx
-<PieChart></PieChart>
+const data=[
+        { x: "Cats", y: 35 },
+        { x: "Dogs", y: 40 },
+        { x: "Birds", y: 55 }
+      ];
+
+<PieChart data={data}/>
 ```
